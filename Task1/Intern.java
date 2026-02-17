c756e9e464577b0a18ee2566aadc5b4fd13a7fd7
pratikshaupasani-sketch <pratiksha.upasani@coditas.com>
2026-02-17 15:57:14 +0530

    Update Intern.java

--- Task1/Intern.java
+++ Task1/Intern.java
@@ -1,19 +1,19 @@
 package Task1;
-
-
-public class Intern extends Employee {
-
+public class Intern extends Employee 
+{
  public Intern(String name, double baseSalary, int attendanceDays, int performanceRating) {
      super(name, baseSalary, attendanceDays, performanceRating);
  }
-
  @Override
  public double calculateGrossSalary() {
      double attendancePercent = (attendanceDays / 30.0) * 100;
      if (attendancePercent < 70) {
          return baseSalary - (baseSalary * 0.20);
-     } else {
+     } 
+     else 
+     {
          return baseSalary;
      }
  }
 }
+
